e205f7ff3761cc2d1968db252a3901bde5ed79c5
RobinCode <xv.robincode@gmail.com>
2022-09-06 09:58:10 +0800

    准备上传到GitHub

---
+++ dist/vue.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="@moddota/panorama-types" />
+declare const render: import('@vue/runtime-core').RootRenderFunction<Panel>,
+    createApp: import('@vue/runtime-core').CreateAppFunction<Panel>;
+export { render, createApp };
+export * from '@vue/runtime-core';
